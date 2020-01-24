9c53819286d1837b16e3d6521668b6b6586ea8ef
Dominik Fomicz <nyhm801@gmail.com>
2020-01-24 18:06:52 +0000

    Revert "logowanie 22.01"
    This reverts commit a7ce88fea7c2984ab1b205293aa6eec3adfaa6ce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,72 +5,111 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Device } from '@ionic-native/device/ngx';
 import { Pages } from './interfaces/pages';
-import { Storage } from '@ionic/storage';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: 'app.component.html',
-	styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-	user_type: any = '0';
-	email: any;
+  user_type: any = '0';
+  email: any;
 
-	public appPages: Array<Pages>;
-	httpOptions = {};
-	constructor(
-		private platform: Platform,
-		private splashScreen: SplashScreen,
-		private statusBar: StatusBar,
-		public navCtrl: NavController,
-		public device: Device,
-		private http: HttpClient,
-		public storage: Storage
-	) {
-	this.appPages = [
+  public appPages: Array<Pages>;
+  httpOptions = {};
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    public navCtrl: NavController,
+    public device: Device,
+    private http: HttpClient
+  ) {
+    this.appPages = [
+      // {
+      //   title: 'Lokale',
+      //   url: '/home-results',
+      //   direct: 'root',
+      //   icon: 'home'
+      // },
+      // {
+      //   title: 'Kupony',
+      //   url: '/tickets',
+      //   direct: 'forward',
+      //   icon: 'barcode'
+      // },
 
-		{
-			title: 'Prześlij uwagi',
-			url: '/contact',
-			direct: 'forward',
-			icon: 'mail'
-		},
+      // {
+      //   title: 'Ulubione',
+      //   url: '/favourites',
+      //   direct: 'forward',
+      //   icon: 'star'
+      // },
 
-		{
-			title: 'O Foodinim',
-			url: '/about',
-			direct: 'forward',
-			icon: 'information-circle'
-		},
-	];
+      // {
+      //   title: 'Mapa',
+      //   url: '/map-card',
+      //   direct: 'forward',
+      //   icon: 'pin'
+      // },
 
-		this.initializeApp();
-	}
+      // {
+      //   title: 'Strona powitalna',
+      //   url: '/welcome-page',
+      //   direct: 'forward',
+      //   icon: 'pin'
+      // }
 
-	initializeApp() {
-	// this.storage.set('name', 'Max');
+      // {
+      //   title: 'Poleć aplikację',
+      //   url: 'https://www.facebook.com/Foodiniapp/',
+      //   direct: 'forward',
+      //   icon: 'share'
+      // },
 
-	this.storage.get('name').then((val) => {
-		console.log('Your name is', val);
-	});
+      {
+        title: 'Prześlij uwagi',
+        url: '/contact',
+        direct: 'forward',
+        icon: 'mail'
+      },
 
-	this.email = this.device.uuid;
-	this.platform.ready().then(() => {
+      {
+        title: 'O Foodinim',
+        url: '/about',
+        direct: 'forward',
+        icon: 'information-circle'
+      },
+    ];
+    // if(this.platform.pause){
+    //   localStorage.clear();
+    // }
+    this.initializeApp();
+  }
 
+  initializeApp() {
+    // this.user_type = localStorage.getItem('user_type');
+    this.email = this.device.uuid;
+    this.platform.ready().then(() => {
+      this.statusBar.styleBlackTranslucent();
+      this.splashScreen.hide();
+      this.email = this.device.uuid;
+      const post_data = new HttpParams()
+			.set('uuid', this.device.uuid);
 
+		return this.http.post('http://repo.foodini.net.pl/auth-api/getUserStatus', post_data, this.httpOptions).subscribe(data=>{
+      this.user_type = data;
+    });
 
-		this.statusBar.styleBlackTranslucent();
-		this.splashScreen.hide();
-	// 	this.email = this.device.uuid;
-	// 	const post_data = new HttpParams()
-	// 		.set('uuid', this.device.uuid);
+    }).catch(() => {});
+  }
 
-	// 	return this.http.post('http://repo.foodini.net.pl/auth-api/getUserStatus', post_data, this.httpOptions).subscribe(data=>{
-	// 	this.user_type = data;
-	// });
-		this.user_type = 3;
+  goToEditProgile() {
+    this.navCtrl.navigateForward('edit-profile');
+  }
 
-	}).catch(() => {});
-	}
+  logout() {
+    this.navCtrl.navigateRoot('/');
+  }
 }
